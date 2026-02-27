96a119f3a6ab0d935bc970f6dbeed78b77d6c6b4
gilmartinsgomes92 <gilmartinsgomes92@gmail.com>
2026-02-27 22:25:19 +0000

    Update AppContext.tsx to use type-only import for ReactNode and properly type Action interface.

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,66 +1,7 @@
-import { createContext, useContext, useReducer, type ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
 interface Action {
-    type: string;
-    payload?: any;
+  type: string;
 }
 
-interface AppState {
-    user: any;
-    isSubscribed: boolean;
-    tuningSession: any;
-    navigation: string;
-}
-
-const initialState: AppState = {
-    user: null,
-    isSubscribed: false,
-    tuningSession: null,
-    navigation: '',
-};
-
-const appReducer = (state: AppState, action: Action): AppState => {
-    switch (action.type) {
-        case 'SET_USER':
-            return { ...state, user: action.payload };
-        case 'SET_SUBSCRIPTION':
-            return { ...state, isSubscribed: action.payload };
-        case 'SET_TUNING_SESSION':
-            return { ...state, tuningSession: action.payload };
-        case 'SET_NAVIGATION':
-            return { ...state, navigation: action.payload };
-        default:
-            return state;
-    }
-};
-
-interface AppContextType {
-    state: AppState;
-    dispatch: React.Dispatch<Action>;
-}
-
-const AppContext = createContext<AppContextType | undefined>(undefined);
-
-interface AppProviderProps {
-    children: ReactNode;
-}
-
-export const AppProvider = ({ children }: AppProviderProps) => {
-    const [state, dispatch] = useReducer(appReducer, initialState);
-
-    return (
-        <AppContext.Provider value={{ state, dispatch }}>
-            {children}
-        </AppContext.Provider>
-    );
-};
-
-export const useAppContext = () => {
-    const context = useContext(AppContext);
-    if (context === undefined) {
-        throw new Error('useAppContext must be used within an AppProvider');
-    }
-    return context;
-};
-
-export default AppContext;
+// The rest of your AppContext.tsx code should be placed here.
