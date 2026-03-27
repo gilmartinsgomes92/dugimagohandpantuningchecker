1198610a4ef3d8519ba30ff22af1c93aac4b73a7
gilmartinsgomes92 <gilmartinsgomes92@gmail.com>
2026-03-27 19:14:06 +0000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AppProvider } from './contexts/AppContext';
+import { CertificationProvider } from './contexts/CertificationContext';
+import WelcomePage from './pages/WelcomePage';
+import NotesCountSelectionPage from './pages/NotesCountSelectionPage';
+import QuickTuningPage from './pages/QuickTuningPage';
+import ScaleSelectionPage from './pages/ScaleSelectionPage';
+import ScaleIdentifyPage from './pages/ScaleIdentifyPage';
+import GuidedTuningPage from './pages/GuidedTuningPage';
+import ResultsDashboardPage from './pages/ResultsDashboardPage';
+import ContactFormPage from './pages/ContactFormPage';
+import ConfirmationPage from './pages/ConfirmationPage';
+import StrobeTuningPage from './pages/StrobeTuningPage';
+import IdentifyNotePage from './pages/IdentifyNotePage';
+import TuningStartPage from './pages/TuningStartPage';
+import CertificationStartPage from './pages/CertificationStartPage';
+import CertificationCheckPage from './pages/CertificationCheckPage';
+import CertificationResultsPage from './pages/CertificationResultsPage';
+import './App.css';
+
+function App() {
+  return (
+    <AppProvider>
+      <CertificationProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/notes-count-selection" element={<NotesCountSelectionPage />} />
+          <Route path="/quick-tuning" element={<QuickTuningPage />} />
+          <Route path="/scale-selection" element={<ScaleSelectionPage />} />
+          <Route path="/scale-identify" element={<ScaleIdentifyPage />} />
+          <Route path="/guided-tuning" element={<GuidedTuningPage />} />
+          <Route path="/results" element={<ResultsDashboardPage />} />
+          <Route path="/contact" element={<ContactFormPage />} />
+          <Route path="/confirmation" element={<ConfirmationPage />} />
+          {/* Legacy routes (kept for backward compatibility) */}
+          <Route path="/strobe-tuning" element={<StrobeTuningPage />} />
+          <Route path="/identify-note" element={<IdentifyNotePage />} />
+          {/* 2-step guided tuning workflow */}
+          <Route path="/tuning/start" element={<TuningStartPage />} />
+          <Route path="/tuning/identify-note" element={<IdentifyNotePage />} />
+          <Route path="/tuning/strobe-measurement" element={<StrobeTuningPage />} />
+          <Route path="/tuning/results" element={<ResultsDashboardPage />} />
+          <Route path="/certification/start" element={<CertificationStartPage />} />
+          <Route path="/certification/check" element={<CertificationCheckPage />} />
+          <Route path="/certification/results" element={<CertificationResultsPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+      </CertificationProvider>
+    </AppProvider>
+  );
+}
+
+export default App;
