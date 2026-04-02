4f02ee607667950fbe9d217ffedb6626f6fdcc99
gilmartinsgomes92 <gilmartinsgomes92@gmail.com>
2026-04-02 13:53:53 +0100

    Refactor NOTE_COUNTS to use dynamic array generation

--- src/pages/TuningStartPage.tsx
+++ src/pages/TuningStartPage.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '../contexts/AppContext';
 
-const NOTE_COUNTS = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const NOTE_COUNTS = Array.from({ length: 24 }, (_, index) => index + 7);
 
 const TuningStartPage: React.FC = () => {
   const navigate = useNavigate();
